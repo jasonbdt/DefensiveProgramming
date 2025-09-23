cea4498921e8670542960640406adccc16918343
jasonbdt <ja.bladt@gmail.com>
2025-09-23 13:13:57 +0200

    Add function body for number_from_list_of_digits

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 
 
-def number_from_list_of_digits(list_of_digits: list[int]):
+def number_from_list_of_digits(list_of_digits: list[int]) -> int:
     """
     This function gets a list of int digits, like [1, 4, 0]
     And returns the number made out of the digits, as int (140).
     If there is a problem with input values, raises a ValueError.
     If there is a problem with input types, raises a TypeError.
     """
-    pass
+    output = ""
+    for digit in list_of_digits:
+        output += str(digit)
+
+    return int(output)
 
 
 def main() -> None:
-    pass
+    result = number_from_list_of_digits([1, 4, 0])
+    print(result)
 
 
 if __name__ == '__main__':
