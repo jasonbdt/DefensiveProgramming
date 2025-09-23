cadea83b52e3da81de9c3a9c5e490b0a8e35712f
jasonbdt <ja.bladt@gmail.com>
2025-09-23 13:11:36 +0200

    Add blueprint for number_from_list_of_digits

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 
 
+def number_from_list_of_digits(list_of_digits: list[int]):
+    """
+    This function gets a list of int digits, like [1, 4, 0]
+    And returns the number made out of the digits, as int (140).
+    If there is a problem with input values, raises a ValueError.
+    If there is a problem with input types, raises a TypeError.
+    """
+    pass
+
+
 def main() -> None:
     pass
 
